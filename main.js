2a643392556d6c2e62ef74c2dfdcffeb029983fe
Zeglol1234 <95651912+zeglol1234@users.noreply.github.com>
2023-09-29 12:19:14 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+	document.getElementById("cookie").addEventListener("click", function () {
+		location.replace("Cookie-Clicker-2.048");
+	});
+	document.getElementById("dext").addEventListener("click", function () {
+		location.replace("dextensify.html");
+	});
+	document.getElementById("home").addEventListener("click", function () {
+		location.replace("/");
+	});
+	document.getElementById("blog").addEventListener("click", function () {
+		location.replace("/blog");
+	});
+};
